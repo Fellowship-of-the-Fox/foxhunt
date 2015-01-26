606f635dc860f09c9a508221e319fffcba5aed41
Walker Malling <walker.malling@gmail.com>
2015-01-25 23:34:22 -0800

    apply basic color and alignment

--- home.php
+++ home.php
@@ -1,3 +1,23 @@
-<div class="main">
-  <h1>Welcome to the Hunt</h1>
-</div>
+<header>
+  <h3>The Fellowship of the Fox</h3>
+  <h3>At Purlieu Hall</h3>
+  <h3>Presents</h3>
+  <h3>The First Annual</h3>
+  <h1>Fox Hunt</h1>
+  <h3>Volunteer</h3>
+  <h3>Park</h3>
+  <h3>Seattle</h3>
+</header>
+
+<main>
+  <ul>
+    <li><a>About the Hunt</a></li>
+    <li><a>Register</a></li>
+    <li><a>Friends of the Hunt</a></li>
+    <li><a>Contact</a></li>
+  </ul>
+</main>
+
+<footer>
+  <address>102 South Jackson St Seattle</address>
+</footer>
